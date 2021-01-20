e7a62aea99d77b42af9c6ff9264d14ec17157fae
brayanlml <lopez.vicio.2b@gmail.com>
2021-01-20 20:51:38 -0300

    Se creo pagina de inicio

--- trabajo2/pelicula/views.py
+++ trabajo2/pelicula/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Pelicula,Genero,Author
 
 # Create your views here.
+
+def index(request):
+    
+    num_pelicula = Pelicula.objects.all().count()
+    num_authors = Author.objects.count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_pelicula': num_pelicula,'num_authors': num_authors},
+    )
