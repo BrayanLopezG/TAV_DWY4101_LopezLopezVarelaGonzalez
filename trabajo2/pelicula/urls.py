c2f6cbfa10176871b8a41ea5e3be3b1d1c04e782
brayanlml <lopez.vicio.2b@gmail.com>
2021-01-21 21:33:11 -0300

    Version 1.4

--- trabajo2/pelicula/urls.py
+++ trabajo2/pelicula/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns=[ 
     path('', views.index, name='index'),
+    path('pelicula/<int:pk>', views.PeliculaDetailView.as_view(), name='pelicula-detail'),
+]
+
+urlpatterns+=[
+    path('pelicula/create/', views.PeliculaCreate.as_view(), name='pelicula_create'),
+    path('pelicula/<int:pk>/update/', views.PeliculaUpdate.as_view(), name='pelicula_update'),
+    path('pelicula/<int:pk>/delete/', views.PeliculaDelete.as_view(), name='pelicula_delete'),
 ]
--- trabajo2/pelicula/views.py
+++ trabajo2/pelicula/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Pelicula,Genero,Author
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
+from django.views import generic
 
 # Create your views here.
 
@@ -13,3 +16,18 @@ def index(request):
         'index.html',
         context={'num_pelicula': num_pelicula,'num_authors': num_authors},
     )
+
+class PeliculaCreate(CreateView):
+    model = Pelicula
+    fields = '__all__'
+
+class PeliculaUpdate(UpdateView):
+    model = Pelicula
+    fields = ['titulo','descripcion','autor','genero',]
+
+class PeliculaDelete(DeleteView):
+    model = Pelicula
+    success_url = reverse_lazy('peliculas')
+
+class PeliculaDetailView(generic.DetailView):
+    model = Pelicula
